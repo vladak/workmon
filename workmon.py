b07018edb6dbaf6018a20a847caf05469f58a7f7
Vladimir Kotal <vlada@kotalovi.cz>
2022-02-02 15:23:45 +0100

    continue if display state cannot be determined

--- workmon.py
+++ workmon.py
@@ -116,8 +116,9 @@ def sensor_loop(timeout, display, table, bulb, maximums):
         )  # Unlike display, not interested in actual position.
         gauges[table_gauge].set(int(table_state))
         display_on = display.is_on()
-        if display_on:
-            gauges[display_gauge].set(int(display_on))
+        if display_on is None:
+            continue
+        gauges[display_gauge].set(int(display_on))
 
         # How much time in seconds has elapsed since the last loop iteration.
         # TODO: for now run with this estimate
