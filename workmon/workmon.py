50cce36126989f6dc20e60bac921ff5682e35bc9
Vladimir Kotal <vlada@kotalovi.cz>
2022-02-02 22:38:55 +0100

    use NaN when display state is unknown
    fixes #1

--- workmon/workmon.py
+++ workmon/workmon.py
@@ -121,6 +121,7 @@ def sensor_loop(timeout, display, table, bulb, maximums):
         gauges[table_gauge].set(int(table_state))
         display_on = display.is_on()
         if display_on is None:
+            gauges[display_gauge].set('NaN')
             continue
         gauges[display_gauge].set(int(display_on))
 
