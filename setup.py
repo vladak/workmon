172147968e9db770a418a93da7d21581518cc8c2
Vladimir Kotal <vlada@kotalovi.cz>
2022-02-02 18:06:32 +0100

    apply black

--- setup.py
+++ setup.py
@@ -1,20 +1,21 @@
 from setuptools import setup
 
 setup(
-   name='workmon',
-   version='0.1',
-   description='Work monitoring',
-   author='Vladimir Kotal',
-   author_email='vlada@kotalovi.cz',
-   packages=['workmon'],
-   install_requires=['pyserial', 'adafruit-circuitpython-us100', 'prometheus-client', 'PyP100'],
-   entry_points={
-      'console_scripts': [
-         'workmon = workmon.workmon:main'
-      ]
-   },
-   tests_require=[
-      'pytest',
-      'pytest-cov',
-   ],
+    name="workmon",
+    version="0.1",
+    description="Work monitoring",
+    author="Vladimir Kotal",
+    author_email="vlada@kotalovi.cz",
+    packages=["workmon"],
+    install_requires=[
+        "pyserial",
+        "adafruit-circuitpython-us100",
+        "prometheus-client",
+        "PyP100",
+    ],
+    entry_points={"console_scripts": ["workmon = workmon.workmon:main"]},
+    tests_require=[
+        "pytest",
+        "pytest-cov",
+    ],
 )
