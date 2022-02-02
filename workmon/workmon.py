401bfd36292fda61983547838972df41eb8b4a2e
Vladimir Kotal <vlada@kotalovi.cz>
2022-02-02 22:42:04 +0100

    apply black

--- workmon/workmon.py
+++ workmon/workmon.py
@@ -15,44 +15,7 @@ from .bulb import Bulb
 from .display import Display, DisplayException
 from .parserutil import parse_args
 from .table import Table
-
-
-def time_delta_fmt(seconds):
-    """
-    :return number of seconds formatted for logging
-    """
-    if seconds < 60:
-        return f"{seconds}s"
-
-    if seconds < 3600:
-        minutes, seconds = divmod(seconds, 60)
-        return f"{minutes}:{seconds:02}"
-
-    hours, remainder = divmod(seconds, 3600)
-    minutes, seconds = divmod(remainder, 60)
-    return f"{hours}:{minutes:02}:{seconds:02}"
-
-
-def get_tty_usb(id_to_match):
-    """
-    Find device tree entry for given ID/substring. Normally this should return /dev/ttyUSB*
-    """
-
-    logger = logging.getLogger(__name__)
-
-    by_id_dir_path = "/dev/serial/by-id/"
-    if not os.path.isdir(by_id_dir_path):
-        raise OSError(f"need {by_id_dir_path} directory to work")
-
-    for item in os.listdir(by_id_dir_path):
-        if id_to_match in item:
-            logger.debug(f"found a match for {id_to_match}: {item}")
-            link_destination = os.readlink(os.path.join(by_id_dir_path, item))
-            return os.path.realpath(
-                os.path.join("/dev/serial/by-id/", link_destination)
-            )
-
-    return None
+from .utils import get_tty_usb, time_delta_fmt
 
 
 # pylint: disable=too-few-public-methods
@@ -121,7 +84,7 @@ def sensor_loop(timeout, display, table, bulb, maximums):
         gauges[table_gauge].set(int(table_state))
         display_on = display.is_on()
         if display_on is None:
-            gauges[display_gauge].set('NaN')
+            gauges[display_gauge].set("NaN")
             continue
         gauges[display_gauge].set(int(display_on))
 
