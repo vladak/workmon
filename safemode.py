e9e586e83e894ca1853c6c0e66d323ae904ef766
Vladimir Kotal <vlada@kotalovi.cz>
2024-04-16 21:35:29 +0200

    handle safe mode entered as a result of hard fault
    fixes #25

---
+++ safemode.py
@@ -0,0 +1,8 @@
+import microcontroller
+import supervisor
+
+if supervisor.runtime.safe_mode_reason == supervisor.SafeModeReason.HARD_FAULT:
+    microcontroller.reset()  # Reset and start over.
+
+# Otherwise, do nothing. The safe mode reason will be printed in the
+# console, and nothing will run.
