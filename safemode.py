56ae2a9b5405661cf121952c44123cf7a7f20645
Vladimir Kotal <vlada@kotalovi.cz>
2024-04-16 22:01:42 +0200

    handle pylint

--- safemode.py
+++ safemode.py
@@ -1,10 +1,14 @@
 """
 safe mode handling
 """
+
 import microcontroller
+
+# pylint: disable=import-error
 import supervisor
 
 if supervisor.runtime.safe_mode_reason == supervisor.SafeModeReason.HARD_FAULT:
+    # pylint: disable=no-member
     microcontroller.reset()  # Reset and start over.
 
 # Otherwise, do nothing. The safe mode reason will be printed in the
