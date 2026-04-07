319b22c914a80c52562e7a059c08f889492901a1
Vladimir Kotal <vlada@kotalovi.cz>
2026-04-07 10:04:01 +0200

    handle internal watchdog timeout
    Sometimes the microcontroller throws internal watchdog timeout. Handle this by resetting.

--- safemode.py
+++ safemode.py
@@ -10,6 +10,9 @@ import supervisor
 if supervisor.runtime.safe_mode_reason == supervisor.SafeModeReason.HARD_FAULT:
     # pylint: disable=no-member
     microcontroller.reset()  # Reset and start over.
+elif reason == supervisor.SafeModeReason.WATCHDOG:
+    # pylint: disable=no-member
+    microcontroller.reset()  # Reset and start over.
 
 # Otherwise, do nothing. The safe mode reason will be printed in the
 # console, and nothing will run.
